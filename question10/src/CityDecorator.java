ab312f2685da9928d246d211db4f45b435082117
kwener <123104557+kwener@users.noreply.github.com>
2025-04-18 12:31:44 -0700

    pull

--- question10/src/CityDecorator.java
+++
@@ -1,8 +0,0 @@
-
-public class CityDecorator {
-    public static void main(String[] args) {
-        System.out.println("Base ride service:");
-        RideService baseRide = new RideService();
-        baseRide.getRide();
-    }
-}
--- question10/src/RideService.java
+++
@@ -1,6 +0,0 @@
-public class RideService implements Service {
-    @Override
-    public void getRide() {
-        System.out.println("Getting car service");
-    }
-}
